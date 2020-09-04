48de49940793211c7f68bc25f78736f3d460c564
shahat <me6847570@gmail.com>
2020-09-05 00:55:27 +0200

    the second commit in this time i used mongoose to make code esear and small

--- app.js
+++ app.js
@@ -4,55 +4,65 @@ YEAR : 2020
 this how to use mongodb native to connct to app .js
 LINK : https://mongodb.github.io/node-mongodb-native/3.0/quick-start/quick-start/ 
 Cnclusion :: using native mongodb by this way will waist your time as a developer 
- */
+you can see the code in the first commit on githup 
+*/
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+ // then i will start using mongoose
+ const mongoose=require('mongoose');
+/* these linse to solve the problem that 
+happing whilw working with  */
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'fruitdb';
-
-// Use connect method to connect to the data in mongodp
-MongoClient.connect(url,{ useUnifiedTopology: true } , function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-  const db = client.db(dbName);
-  findDocuments(db, function() {
-    client.close();
-  });
+mongoose.connect(
+    'mongodb://localhost:27017/fruitdb'
+    );
+// creating Schema 
+const fruitSchema = new mongoose.Schema ({
+    name: String,
+    rating:Number,
+    review : String
 });
-/* insert a collection using mongo driver  */
-const insertDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('fruits');
-    // Insert some documents
-    collection.insertMany([
-     {
-         name:"apple",
-         score:8,
-         review:"great fruit"
-     } ,
-     {
-        name:"orange",
-        score:5,
-        review:"kinda sour"
-    } , 
-    {
-        name:"banana",
-        score:9,
-        review:"great fruit "
-    } 
-    ], function(err, result) {
-      assert.equal(err, null);
-      assert.equal(3, result.result.n);
-      assert.equal(3, result.ops.length);
-      console.log("Inserted 3 documents into the collection");
-      callback(result);
-    });
-  }
+const peopleSchema = new mongoose.Schema ({
+    name:String,
+    age: Number
+})
+/*  */
+const Fruit = mongoose.model("Fruit", fruitSchema);
+const People = mongoose.model("People",peopleSchema);
+
+
+const fruit= new Fruit ({
+    name : "apple",
+    rating:6,
+    review:"pretty solid as fruit "
+});
+const orange= new Fruit ({
+    name : "orange",
+    rating:45,
+    review:"pretty solid as fruit "
+});const banana =  new Fruit ({
+    name : "banana",
+    rating:12,
+    review:"pretty solid as fruit "
+});
+
+Fruit.insertMany([{orange},{banana}],function(err){
+    if(err){
+        console.log(err)
+    }else{
+        console.log('succeded')
+    }
+})
+const people = new People({
+    name:"mohamed elshahat ",
+    age : "20"
+})
+
+ // fruit.save();
+  people.save();
+/* coonect to database or create one like the following fruitdb */
+
   /* this code will print the fruits inside your console 
   and then you can deal with the data as you want  */
   const findDocuments = function(db, callback) {
